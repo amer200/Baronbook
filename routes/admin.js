d55b9e4915aa108f754c7d106b48072e179c57b9
amer200 <amermostaafa@gmail.com>
2023-05-31 17:28:06 +0300

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,5 @@ const adminMiddlewares = require("../middlewares/admin");
 routes.post("/login", adminController.logIn);
 routes.post("/add-main-categ", adminMiddlewares.isAdmin, adminController.addMainCateg);
 routes.post("/add-sub-categ", adminMiddlewares.isAdmin, adminController.addSubCateg);
+routes.post("/add-sub-to-main", adminMiddlewares.isAdmin, adminController.addSubToMain);
 module.exports = routes;
